4314f66c7681690e6612f5b4e7fca1210aa5c8a1
Nunchux21 <karlmcaree@gmail.com>
2020-02-21 16:29:44 +0000

    Update run.sh
    changed directories to match https://github.com/tsightler/ring-mqtt new structure

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ echo NPM Version
 npm -v
 git --version
 
-cp /data/options.json ring-alarm-mqtt/config.json
+cp /data/options.json ring-mqtt/config.json
 
 cd ring-alarm-mqtt
 npm install
 npm audit fix
-chmod +x ring-alarm-mqtt.js
-DEBUG=ring-alarm-mqtt /ring-alarm-mqtt/ring-alarm-mqtt.js
+chmod +x ring-mqtt.js
+DEBUG=ring-alarm-mqtt /ring-mqtt/ring-mqtt.js
